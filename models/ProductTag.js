2f1ed25bb74098e9c5c4225697bda9ce1855d0f4
AnthonyDipopolo <anthony.dipopolo@gmail.com>
2023-07-17 20:07:10 -0400

    finished productTag model

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -1,39 +1,39 @@
-const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/connection');
+const { Model, DataTypes } = require('sequelize'); // Import necessary modules from sequelize
+const sequelize = require('../config/connection'); // Import the sequelize connection
 
-class ProductTag extends Model {}
+class ProductTag extends Model {} // Create a ProductTag class that extends the Sequelize Model class
 
 ProductTag.init(
   {
-    // Define columns
+    
     id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true,
+      type: DataTypes.INTEGER, // Data type for the column integer
+      allowNull: false, // The column cannot be null
+      primaryKey: true, // The column is the primary key of the table
+      autoIncrement: true, // The column auto-increments for each new row
     },
     product_id: {
-      type: DataTypes.INTEGER,
+      type: DataTypes.INTEGER, // Data type for the column integer
       references: {
-        model: 'product', // This references the product models id
-        key: 'id',
+        model: 'product', // Reference the product model
+        key: 'id', // Use the id column of the 'product' model as the reference key
       },
     },
     tag_id: {
-      type: DataTypes.INTEGER,
+      type: DataTypes.INTEGER, // Data type for the column integer
       references: {
-        model: 'tag', // This references the tag models id
-        key: 'id',
+        model: 'tag', // Reference the tag model
+        key: 'id', // Use the id column of the 'tag' model as the reference key
       },
     },
   },
   {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'product_tag',
+    sequelize, 
+    timestamps: false, 
+    freezeTableName: true, 
+    underscored: true, 
+    modelName: 'product_tag', 
   }
 );
 
-module.exports = ProductTag;
+module.exports = ProductTag; // Export the ProductTag model

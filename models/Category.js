11867ee3d959ab43669276e6a0179db302a5a9b4
AnthonyDipopolo <anthony.dipopolo@gmail.com>
2023-07-17 20:01:00 -0400

    finished category model

--- models/Category.js
+++ models/Category.js
@@ -1,36 +1,30 @@
-const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/connection.js');
-// const Product = require('./Product');
+const { Model, DataTypes } = require('sequelize'); // Import necessary modules from sequelize
+const sequelize = require('../config/connection.js'); // Import the sequelize connection
 
-
-class Category extends Model {}
+class Category extends Model {} // Create a Category class that extends the Sequelize Model class
 
 Category.init(
   {
-   
+    // Define columns for the Category model
     id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true,
+      type: DataTypes.INTEGER, // Data type for the column integer
+      allowNull: false, // The column cannot be null
+      primaryKey: true, // The column is the primary key of the table
+      autoIncrement: true, // The column auto-increments for each new row
     },
     category_name: {
-      type: DataTypes.STRING,
-      allowNull: false,
+      type: DataTypes.STRING, // Data type for the column string
+      allowNull: false, // The column cannot be null
     },
   },
   {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'category',
+    sequelize, // Pass the sequelize connection to the model
+    timestamps: false, // Disable timestamps createdAt and updatedAt columns
+    freezeTableName: true, // Don't change the table name to plural form
+    underscored: true, // Use underscores instead of camel case for column names
+    modelName: 'category', // Name of the model in singular form used in associations
   }
 );
 
-// Category has many Product models
-// Category.hasMany(Product, {
-//   foreignKey: 'category_id', // This is the foreign key column in the Product model
-// });
 
-module.exports = Category;
+module.exports = Category; // Export the Category model
